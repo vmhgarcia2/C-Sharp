201dcf1d8980083adfffca2d5b7c2f49841273a4
Najabi <35067475+Najabi@users.noreply.github.com>
2019-07-01 21:31:07 +0200

    EjercicioNumPositivosNegativosTerminado

---
+++ 12-NumNegativosPositivos/Program.cs
@@ -0,0 +1,22 @@
+﻿//Cree un programa que a partir de n números introducidos nos índique el número de números positivos introducidos y el número de números negativos introducidos.
+using System;
+
+namespace _12_NumNegativosPositivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numPositivos=0;
+            int numNegativos=0;
+            for(int i=0;i<args.Length;i++){
+                if(args[i].Split("-").Length>1){
+                    numNegativos++;
+                }
+                else{ numPositivos++;}
+            }
+            Console.WriteLine("Numeros negativos: " + numNegativos);
+            Console.WriteLine("Numeros positivos: " + numPositivos);
+        }
+    }
+}
