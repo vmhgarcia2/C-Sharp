1ad9128f048786d6e5ae33c14cd50700d7cc9600
Najabi <35067475+Najabi@users.noreply.github.com>
2019-07-01 20:39:19 +0200

    EjercicioFibonacciTerminado

---
+++ 10-Fibonacci/Program.cs
@@ -0,0 +1,28 @@
+﻿//Cree un programa que a partir de un entero calcule su serie Fibonacci
+using System;
+
+namespace _10_Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length>0){
+                int limite=Convert.ToInt16(args[0]);
+                int numeroActual=0;
+                int numeroAnterior=1;
+                int aux=0;
+                Console.WriteLine(0);
+                do { 
+                    aux=numeroActual;
+                    numeroActual=numeroAnterior;
+                    numeroAnterior=aux+numeroActual;
+                    Console.WriteLine(numeroActual);
+                } while(numeroAnterior<limite);
+            }
+            else{
+                Console.WriteLine("Tienes que introducir un número entero");
+            }
+        }
+    }
+}
