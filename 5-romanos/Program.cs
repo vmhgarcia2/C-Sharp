946772a53546564d7833cb276a917b8b6e565048
Najabi <35067475+Najabi@users.noreply.github.com>
2019-07-01 18:27:22 +0200

    EjercicioNumerosRomanosTerminado

--- 5-romanos/Program.cs
+++ 5-romanos/Program.cs
@@ -6,14 +6,11 @@ namespace _5_romanos
     {
         static void Main(string[] args)
         {
-            //1,2,3,4 I,II,III,IIII
-            //5 V
-            //6,7,8 VI,VII,VIII,VIIII
-            //10 X
             int numero=Convert.ToInt32(args[0]);
             string numeroRomano="";
+            //Millares
             if(Convert.ToInt32(numero)/1000>0){
-                Console.WriteLine(Convert.ToInt32(numero)/1000);
+                //Console.WriteLine(Convert.ToInt32(numero)/1000);
                 switch(Convert.ToInt32(numero)/1000){
                     case 1:
                     case 2:
@@ -23,22 +20,86 @@ namespace _5_romanos
                         break;
                 }
             }
+            //Centenas
             numero=numero%1000;            
             if(Convert.ToInt32(numero)/100>0){
-                Console.WriteLine(Convert.ToInt32(numero)/1000);
-                switch(Convert.ToInt32(numero)/1000){
+                //Console.WriteLine(Convert.ToInt32(numero)/100);
+                switch(Convert.ToInt32(numero)/100){
                     case 1:
                     case 2:
                     case 3:
+                        numeroRomano=numeroRomano.PadRight((Convert.ToInt32(numero)/100+numeroRomano.Length),'C');
+                        break;
                     case 4: 
-                        numeroRomano=(numeroRomano.PadRight(Convert.ToInt32(numero)/1000,'C'));
+                        numeroRomano=numeroRomano+"CD";
+                        break;
+                    case 5:
+                        numeroRomano=numeroRomano+'D';
+                        break;
+                    case 6:
+                    case 7:
+                    case 8:
+                        numeroRomano=numeroRomano+'D';
+                        numeroRomano=numeroRomano.PadRight((((Convert.ToInt32(numero)/100)-5)+numeroRomano.Length),'C');
+                        break;
+                    case 9:                        
+                        numeroRomano=numeroRomano+"CM";
                         break;
                 }
+                 //Decenas
+                numero=numero%100;            
+                if(Convert.ToInt32(numero)/10>0){
+                    switch(Convert.ToInt32(numero)/10){
+                        case 1:
+                        case 2:
+                        case 3:
+                            numeroRomano=numeroRomano.PadRight((Convert.ToInt32(numero)/10+numeroRomano.Length),'X');
+                            break;
+                        case 4: 
+                            numeroRomano=numeroRomano+"XL";
+                            break;
+                        case 5:
+                            numeroRomano=numeroRomano+'L';
+                            break;
+                        case 6:
+                        case 7:
+                        case 8:
+                            numeroRomano=numeroRomano+'L';
+                            numeroRomano=numeroRomano.PadRight((((Convert.ToInt32(numero)/10)-5)+numeroRomano.Length),'X');
+                            break;
+                        case 9:                        
+                            numeroRomano=numeroRomano+"XC";
+                            break;
+                    }
+                }
+                 //Unidades
+                numero=numero%10;            
+                if(Convert.ToInt32(numero)>0){
+                    switch(Convert.ToInt32(numero)){
+                        case 1:
+                        case 2:
+                        case 3:
+                            numeroRomano=numeroRomano.PadRight((Convert.ToInt32(numero)+numeroRomano.Length),'I');
+                            break;
+                        case 4: 
+                            numeroRomano=numeroRomano +"IV";
+                            break;
+                        case 5:
+                            numeroRomano=numeroRomano+'V';
+                            break;
+                        case 6:
+                        case 7:
+                        case 8:                       
+                            numeroRomano=numeroRomano+'V';
+                            numeroRomano=numeroRomano.PadRight((((Convert.ToInt32(numero))-5)+numeroRomano.Length),'I');
+                            break;
+                        case 9:                        
+                            numeroRomano=numeroRomano+"IX";
+                            break;
+                    }
+                }
+            Console.WriteLine("El número " + args[0] + " es " + numeroRomano + " en números romanos");
             }
-            Console.WriteLine(numero);
-            Console.WriteLine(numeroRomano);
-
-            
         }
     }
 }
