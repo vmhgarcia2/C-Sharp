38311299065e911c3f2207064a72d8dd52b26e0c
Najabi <35067475+Najabi@users.noreply.github.com>
2019-07-01 20:02:55 +0200

    TiempoCaidaLibre

---
+++ 9-CalculaTiempoCaida/Program.cs
@@ -0,0 +1,22 @@
+﻿//Escriba un programa que calcule el tiempo de caída de un objeto determinado desde una altura dada, para ello se pide la altura en metros.
+using System;
+
+namespace _9_CalculaTiempoCaida
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length==1){
+                double g=9.81;
+                double altura=Convert.ToDouble(args[0]);
+                double resultado= Math.Sqrt(altura/(g/2));
+                Console.WriteLine("El tiempo que tarda un objeto en caer desde " + altura + " m es de " + resultado + " seg.");
+            }
+            else{
+                Console.WriteLine("Tienes que introducir la altura");
+            }
+            
+        }
+    }
+}
