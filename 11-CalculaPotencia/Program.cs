aba7acce32108d939f3f1dff765032af5a4da716
Najabi <35067475+Najabi@users.noreply.github.com>
2019-07-01 21:08:55 +0200

    EjercicioCalculaPotenciaTerminado

---
+++ 11-CalculaPotencia/Program.cs
@@ -0,0 +1,24 @@
+﻿//Cree un programa que calcule la potencia de un número a partir de su base y su exponente.
+using System;
+
+namespace _11_CalculaPotencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length==2){
+                double bas=Convert.ToDouble(args[0]);
+                double exponente=Convert.ToDouble(args[1]);
+                double total=bas;
+                for(int i=1;i<exponente;i++){
+                    total=bas*total;
+                }
+                Console.WriteLine(bas + "^" + exponente + " = " + total);
+            }
+            else{
+                Console.WriteLine("Tienes que introducir un número entero");
+            }
+        }
+    }
+}
