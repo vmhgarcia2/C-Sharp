88a806a00a69b2023e4afe245e10e8e1f634b668
vmhgarcia2 <52398634+vmhgarcia2@users.noreply.github.com>
2019-07-01 14:56:57 +0200

    numerosRomanosHastaCentenas

--- 5-romanos/Program.cs
+++ 5-romanos/Program.cs
@@ -10,7 +10,35 @@ namespace _5_romanos
             //5 V
             //6,7,8 VI,VII,VIII,VIIII
             //10 X
-            int numero=args[0];
+            int numero=Convert.ToInt32(args[0]);
+            string numeroRomano="";
+            if(Convert.ToInt32(numero)/1000>0){
+                Console.WriteLine(Convert.ToInt32(numero)/1000);
+                switch(Convert.ToInt32(numero)/1000){
+                    case 1:
+                    case 2:
+                    case 3:
+                    case 4: 
+                        numeroRomano=(numeroRomano.PadRight(Convert.ToInt32(numero)/1000,'M'));
+                        break;
+                }
+            }
+            numero=numero%1000;            
+            if(Convert.ToInt32(numero)/100>0){
+                Console.WriteLine(Convert.ToInt32(numero)/1000);
+                switch(Convert.ToInt32(numero)/1000){
+                    case 1:
+                    case 2:
+                    case 3:
+                    case 4: 
+                        numeroRomano=(numeroRomano.PadRight(Convert.ToInt32(numero)/1000,'C'));
+                        break;
+                }
+            }
+            Console.WriteLine(numero);
+            Console.WriteLine(numeroRomano);
+
+            
         }
     }
 }
