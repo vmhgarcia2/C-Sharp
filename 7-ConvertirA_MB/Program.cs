fa808dcc6e9e05b302db280a5608b4d1542abe54
Najabi <35067475+Najabi@users.noreply.github.com>
2019-07-01 18:57:14 +0200

    EjercicioConversionA_MB_Terminado

---
+++ 7-ConvertirA_MB/Program.cs
@@ -0,0 +1,21 @@
+﻿
+//Realizar un programa que no pida un unidad en kb y nos haga la conversión a MB
+using System;
+
+namespace _7_ConvertirkbaMB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double MB=0;
+            if(args.Length>0) { 
+                MB=Convert.ToDouble(args[0])/1000;
+                Console.WriteLine(args[0] + " kb = " + MB + " MB" );
+            }
+            else{
+                Console.WriteLine("Tienes que introducir un número para poder convertirlo");
+            }
+        }
+    }
+}
