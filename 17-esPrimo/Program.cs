a6adfe8ea6240fa827042b96302e66df011acc7e
Najabi <35067475+Najabi@users.noreply.github.com>
2019-07-02 18:38:41 +0200

    EjercicioPalindromoTerminado

--- 17-esPrimo/Program.cs
+++ 17-esPrimo/Program.cs
@@ -8,12 +8,14 @@ namespace _17_esPrimo
     {
         static void Main(string[] args)
         {
-            int numero=Convert.ToInt16(args[0]);
-            if(esPrimo(numero)){
-                Console.WriteLine("El número " + numero + " es primo");
-            }        
-            else{
-                Console.WriteLine("El número " + numero + "  no es primo");
+            if(args.Length>0) {
+                int numero=Convert.ToInt16(args[0]);
+                if(esPrimo(numero)){
+                    Console.WriteLine("El número " + numero + " es primo");
+                }        
+                else{
+                    Console.WriteLine("El número " + numero + "  no es primo");
+                }
             }
         }
 
---
+++ 18-Palindromo/Program.cs
@@ -0,0 +1,30 @@
+﻿//dada una cadena de texto comprobar si es palíndromo o no
+using System;
+
+namespace _18_Palindromo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Introduce una palabra para comprobar si es un palíndromo: ");
+            string palabra=Console.ReadLine();            
+            Console.Write(palabra + (esPalindromo(palabra)?" es un palíndromo":" no es un palíndromo"));
+        }
+
+        static bool esPalindromo(string palabra){
+            if(palabra.Length>2){
+                if(palabra.Substring(0,1)==palabra.Substring(palabra.Length-1,1))
+                {                     
+                    return esPalindromo(palabra.Substring(1,palabra.Length-2));
+                }
+                else { 
+                    return false;
+                }
+            }
+            else {
+                return palabra.Substring(0,1)==palabra.Substring(palabra.Length-1,1);
+            }
+        }
+    }
+}
