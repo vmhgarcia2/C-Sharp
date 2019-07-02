e01fc06c749f25b0188b96c60218fd50767abf72
Najabi <35067475+Najabi@users.noreply.github.com>
2019-07-02 19:24:46 +0200

    EjercicioCuadradoTerminado

--- 02072017/19-Cuadrado/Program.cs
+++ 02072017/19-Cuadrado/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿//dibuja un cuadrado por consola con las dimensiones que introduzca el usuario ( filas y columnas)
+using System;
 
 namespace _19_Cuadrado
 {
@@ -6,7 +7,37 @@ namespace _19_Cuadrado
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Introduce las anchura del cuadrado: ");
+            int filas=Convert.ToInt16(Console.ReadLine());
+            Console.Write("Introduce las altura del cuadrado: ");
+            int columnas=Convert.ToInt16(Console.ReadLine());
+            string[,] cuadrado=new string[filas,columnas];
+            int i=0;
+            int j=0;
+            for(i=0;i<filas;i++){
+                for(j=0;j<columnas;j++){
+                    if(i==0 || i==filas-1){cuadrado[i,j]="* ";}
+                    if(j==0){
+                        cuadrado[i,j]="* ";
+                    }
+                    else {
+                        if(j==columnas-1){
+                            cuadrado[i,j]="*";
+                        }
+                        else{
+                            if(cuadrado[i,j]!="* ") {
+                                cuadrado[i,j]="  ";
+                            }
+                        }
+                    }
+                }
+            }
+            for(i=0;i<filas;i++){
+                for(j=0;j<columnas;j++){
+                    Console.Write(cuadrado[i,j]);
+                }
+                Console.WriteLine("");
+            }
         }
     }
 }
