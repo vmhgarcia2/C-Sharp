0a1653b054ad30d76a977b7107c8adf2c2ea5501
Najabi <35067475+Najabi@users.noreply.github.com>
2019-07-02 20:06:01 +0200

    Ejercicio>18Terminado

---
+++ 02072017/21-EsMayor18/Program.cs
@@ -0,0 +1,23 @@
+﻿//Dado un array de edades mostrar por pantalla si todas son mayores de 18 o no.
+using System;
+
+namespace _21_EsMayor18
+{
+    class Program
+    {
+            static void Main(string[] args)
+        {
+            int[] edades=new int[]{19,21,23,3,53};
+            int i=0;
+            for(i=0;i<edades.Length;i++){
+                if(edades[i]<18){
+                    Console.WriteLine("Todos los números del array no son mayores de 18");
+                    i=(edades.Length+1);
+                }
+            }
+            if(i==edades.Length){
+                Console.WriteLine("Todos los números del array son mayores de 18");
+            }
+        }
+    }
+}
