9cbeb8c07493cf9e91245464dfc85cec8563aaac
Najabi <35067475+Najabi@users.noreply.github.com>
2019-07-02 20:14:57 +0200

    EjercicioMediaTerminado

---
+++ 02072017/22-media/Program.cs
@@ -0,0 +1,27 @@
+﻿//Permite al usuario introducir 10 notas ( sin utilizar los argumentos) y calcula la media
+using System;
+
+namespace _3_media
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fin=10;
+            double[] numeros=new double[fin];
+            for (int i=0;i<fin;i++){
+                Console.Write("Introduce 10 números (" +  (i+1)  + " de " + fin + "): ");
+                numeros[i]=Convert.ToDouble(Console.ReadLine());
+            }
+            Console.WriteLine(media(numeros));
+        }
+
+        static double media(double[] args){
+            double suma=0;
+            foreach(double valor in args){
+                suma+=valor;
+            }
+            return suma/args.Length;
+        }
+    }
+}
