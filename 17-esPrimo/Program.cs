3ddae42c97341650ae5d5f22f769de34ea27dab7
vmhgarcia2 <52398634+vmhgarcia2@users.noreply.github.com>
2019-07-02 14:50:49 +0200

    NumeroPrimo

---
+++ 17-esPrimo/Program.cs
@@ -0,0 +1,17 @@
+﻿
+//dado un numero introducido por el usuario devovler si es primo o no
+using System;
+
+namespace _17_esPrimo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero=Convert.ToInt16(args[0]);
+            if(numero%numero==0){
+
+            }
+        }
+    }
+}
