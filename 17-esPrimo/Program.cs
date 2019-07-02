c03e2c2ff122d0b7ebb78466450dd9594371eb4d
vmhgarcia2 <52398634+vmhgarcia2@users.noreply.github.com>
2019-07-02 14:58:03 +0200

    EjercicioEsPrimoTerminado

--- 17-esPrimo/Program.cs
+++ 17-esPrimo/Program.cs
@@ -9,9 +9,21 @@ namespace _17_esPrimo
         static void Main(string[] args)
         {
             int numero=Convert.ToInt16(args[0]);
-            if(numero%numero==0){
-
+            if(esPrimo(numero)){
+                Console.WriteLine("El número " + numero + " es primo");
+            }        
+            else{
+                Console.WriteLine("El número " + numero + "  no es primo");
             }
+        }
+
+        static bool esPrimo(int numero){            
+            for(int i=2;i<numero;i++){
+                if(numero%i==0){
+                    return false;
+                }
+            }
+            return true;
         }
     }
 }
