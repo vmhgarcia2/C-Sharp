27f709655be9beb226a4bc5ffd8165961cbedb2c
vmhgarcia2 <52398634+vmhgarcia2@users.noreply.github.com>
2019-07-08 13:46:24 +0200

    Diccionario

---
+++ 08072019/Diccionario/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Diccionario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> diccionario = new Dictionary<string, string>();
+            ConsoleKeyInfo opcion;
+            do
+            {
+                Console.WriteLine("Menu");
+                Console.WriteLine("=====");
+                Console.WriteLine("1. Introducir palabra y definición");
+                Console.WriteLine("2. Buscar palabra");
+                Console.WriteLine("3. Salir");
+                Console.WriteLine("");
+                bool insertada;
+                bool encontrada;
+                opcion = Console.ReadKey();
+                switch (opcion.KeyChar)
+                {
+                    case '1':
+                        do
+                        {
+                            insertada = IntroducePalabra(diccionario);
+                        }
+                        while (insertada);
+                        break;
+                    case '2':
+                        encontrada = bPalabra(diccionario);
+                        break;
+                    case '3':
+                        break;
+                    default:
+                        Console.WriteLine("No has introducido una opción correcta. Vuelve a intentarlo.");
+                        Console.WriteLine("");
+                        break; 
+                }
+            }
+            while (opcion.KeyChar!= '3');
+
+        }
+        public static bool IntroducePalabra(Dictionary<string, string> d)
+        {
+
+            string palabra;
+            string definicion;
+            Console.WriteLine("");
+            Console.Write("Introduce palabra:");
+            palabra = Console.ReadLine();
+            if (palabra.Length > 0)
+            {
+                Console.Write("Introduce definicion:");
+                definicion = Console.ReadLine();
+                try {
+                    d.Add(palabra.ToLower(), definicion);
+                }
+                catch(ArgumentException)
+                {
+                    Console.WriteLine("No se ha podido añadir la palabra");
+                    return true;
+                }
+                Console.WriteLine("");
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    
+        public static bool bPalabra(Dictionary<string, string> d)
+        {
+            string palabra;
+            string valor;
+            Console.Write("Introduce palabra:");
+            palabra = Console.ReadLine();
+            if (palabra.Length > 0)
+            {
+                bool encotrado = d.TryGetValue(palabra.ToLower(), out valor);
+                Console.Write("Definición de " + palabra + ": ");
+                Console.Write(valor);
+                Console.WriteLine("");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("");
+                return false;
+            }
+        }
+    }
+}
---
+++ 08072019/PilasyColas/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace PilasyColas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue <int> cola = new Queue<int>();
+            int suma = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                cola.Enqueue(i);
+            }
+
+            while (cola.Count > 0){
+                suma += cola.Dequeue();
+            }
+
+            Console.WriteLine(suma);
+        }
+    }
+}
